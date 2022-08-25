a169dc8413790f785731d03babca2fe60524e9fa
Muhammed Ali ASLAN <mali.aslan.cyp@gmail.com>
2022-08-25 22:28:56 +0300

    kodlamaioTask

---
+++ course/src/course/Courses.java
@@ -0,0 +1,16 @@
+package course;
+
+public class Courses {
+	
+	public Courses (String name, String instructor, int progress) {
+		
+		this.instructor=instructor;
+		this.name=name;
+		this.progress=progress;
+		
+		
+	}
+	String name;
+	String instructor;
+	int progress;
+}
---
+++ course/src/course/Main.java
@@ -0,0 +1,36 @@
+package course;
+
+import java.util.Iterator;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Courses course1 = new Courses("Yazılım Geliştrici Yetişirme Kampı(c#, Angular)","Engin Demirog",9);
+		
+		Courses course2 = new Courses("Yazılım Geliştrici Kampı(JAVA, REACT)","Engin Demirog", 19);
+		
+		Courses course3 = new Courses("Yazılım Geliştrici Kampı(JavaScript)","Engin Demirog", 29);
+		
+		
+	
+			Courses [] courses = {course1,course2,course3};
+			
+			for	(Courses course : courses) {
+				
+				System.out.println(course.name +" "+ course.instructor +" Yüzde "+ course.progress+ " tamamlandı.");
+				
+			
+				
+				
+				
+			}
+			
+				 
+			 
+			 
+		 
+	
+	}
+
+}
